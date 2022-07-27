c4f15251ece2493d3fdfc968cebb5cfefb3f805b
Madhumida <smadhumida@gmail.com>
2022-07-26 20:06:15 -0700

    ALters home page

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.places_index, name='index'),
     path('about/', views.about, name='about'),
     path('places/', views.places_index, name='index'),
     path('places/<int:place_id>/', views.places_detail, name='detail'),
